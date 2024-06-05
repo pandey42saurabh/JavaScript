07020aa39234348051b2b357fcc6a942f8ebb0ff
pandey42saurabh <91654587+pandey42saurabh@users.noreply.github.com>
2024-06-05 10:09:41 +0000

    typeConversion

---
+++ basics/typeConversion.js
@@ -0,0 +1,26 @@
+let score="33abs"
+
+// console.log(typeof score);
+let changeVariable= Number(score);
+// console.log(typeof changeVariable);
+// console.log(changeVariable);
+
+//  "33" =>33
+// "33abc"=> NaN
+// Null=>0
+// undefined=> Null
+// true =>1: false=> 0
+
+let isLoggedIn=1;
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true || 0 => false
+// "" => false
+// "abc" => true
+
+let someNumber=33
+
+let stringConvert = String(someNumber)
+console.log(stringConvert);
+console.log(typeof(stringConvert));
