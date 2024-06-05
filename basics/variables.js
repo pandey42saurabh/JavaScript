63935931c5d2db5a7f028271c5076c706273010d
pandey42saurabh <91654587+pandey42saurabh@users.noreply.github.com>
2024-06-05 08:13:51 +0000

    learn about variables lvar,let and const

---
+++ basics/variables.js
@@ -0,0 +1,16 @@
+const accountId= 1234
+let accountEmail="saurabh@gmail.com"
+var accountPass="123abc"
+accountCity="Gudgawn"
+
+accountEmail="sp@gmail.com"
+accountPass="1212121"
+accountCity="Noida"
+
+
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPass);
+// console.log(accountCity);
+
+console.table([accountId,accountEmail,accountPass,accountCity]);
