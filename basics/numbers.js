8b13d7e30b71dc8c8e65c676af87a4e6f0631920
pandey42saurabh <91654587+pandey42saurabh@users.noreply.github.com>
2024-06-06 08:26:22 +0000

    Math and Number functions of JavaScript

---
+++ basics/numbers.js
@@ -0,0 +1,19 @@
+const balance =new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const anotherNumber = 123.4567
+// console.log(anotherNumber.toPrecision(1));
+
+const hundreds= 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++Maths++++++++++++++
+
+console.log(Math)
+console.log(Math.abs(-4));
+console.log(Math.round(4.7));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
