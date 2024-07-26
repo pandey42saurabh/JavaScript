bfa5ae7f1233d0fb4648e596516298c8aec82c60
pandey42saurabh <91654587+pandey42saurabh@users.noreply.github.com>
2024-07-26 15:26:39 +0000

    BMI result

--- projects/bmi calculator/bmi.js
+++ projects/bmi calculator/bmi.js
@@ -5,8 +5,14 @@ form.addEventListener('submit',function(e){
     const height=parseInt(document.querySelector('#height').value);
     const weight=parseInt(document.querySelector('#weight').value);
     const results= document.querySelector('#results');
+    const uw = document.querySelector(".under-weight");
+    const nr = document.querySelector(".normal-range");
+    const ow = document.querySelector(".over-weight")
+    function refresh(){
+        window.location.reload ();
+    }
 
-    if(height==='' || height<=0|| isNaN(height)){
+    if(height ==='' || height <=0 || isNaN(height)){
         results.innerHTML =`please provide valid height ${height}`;
     }
     // 
@@ -16,7 +22,20 @@ form.addEventListener('submit',function(e){
     else{
         const bmi = (weight / ((height * height) / 10000)).toFixed(2);
         results.innerHTML = `${bmi}`;
+        if(bmi<18.6){
+            // console.log("under weight");
+            uw.innerHTML= "Under weight"
+            
+        }else if (bmi >=18.6 && bmi <24.9){
+            console.log("Normal weight");
+            nr.innerHTML = "Normal Weight"
+        } else{
+            console.log("over weight");
+            ow.innerHTML="Over Weight"
+        }
     }
+
+    refresh();
 })
 
 
